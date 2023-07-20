e78d34035f69370c93dbadcf3a50fea47cc49c66
Silvan Schmidt <sil.schmidt@proton.me>
2023-07-20 11:08:03 +0200

    init

---
+++ configuration.nix
@@ -0,0 +1,137 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "silva-nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "ch";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "sg";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.silva = {
+    isNormalUser = true;
+    description = "Silvan Schmidt";
+    shell = pkgs.zsh;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      helix
+      zellij
+
+      rustup
+
+      starship
+
+      firefox
+
+      godot_4
+
+      gh
+    ];
+  };
+
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+
+    wget
+
+    unzip
+    zip
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      kitty
+      dmenu
+
+      swaylock
+      swayidle
+    ];
+  };
+
+  # List services that you want to enable:
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "greeter";
+      };
+    };
+  };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir -p ~/.config
+
+# Install gtk config
+cp -r ./dotfiles/gtk-3.0 ~/.config
+
+# Install nixos configs
+sudo cp ./configuration.nix ./hardware-configuration.nix /etc/nixos
+
+# Install starship config
+cp ./dotfiles/starship.toml ~/.config/starship.toml
+
+# Install kitty config
+cp -r ./dotfiles/kitty ~/.config
+
+cp -r ./dotfiles/helix ~/.config
+
+# Install zsh config
+cp ./dotfiles/zshrc ~/.zshrc
+
+# Sway
+cp -r ./dotfiles/sway ~/.config
+wget -O ~/.config/sway/mandelbrot_gap_green.png https://raw.githubusercontent.com/Gingeh/wallpapers/main/mandelbrot/mandelbrot_gap_green.png
